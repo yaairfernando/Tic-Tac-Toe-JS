0bb132ae7b5892ea0c06b840cb868ab45f846320
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-04-08 18:13:03 -0500

    Added win, displayPlayer and displayWiner methods

--- js/script.js
+++ js/script.js
@@ -15,12 +15,22 @@ const gameBoard = (() => {
     9: document.getElementById("9"),
   };
 
+  let boardArray = []
+
   const checkSquare = (num) => squares[num].innerHTML == "";
-  const fillSquare = (player, num) => (squares[num].innerHTML = player.mark);
+  const fillSquare = (player, num) => {
+    squares[num].innerHTML = player.mark;
+    let move = {
+      name: player.name,
+      num
+    }
+    boardArray.push(move);
+  };
 
   return {
     fillSquare,
     checkSquare,
+    boardArray
   };
 })();
 
@@ -30,12 +40,36 @@ const gameController = (() => {
   let turn;
   let brd;
 
+  const displayPlayer = (player) => {
+    let span = document.createElement('span').innerText = `It is ${player.name}'s turn`;
+    document.querySelector('.player').innerHTML = span;
+  }
+
+  const displayWiner = (player) => {
+    let span = document.createElement('span').innerText = `The winner is ${player}`;
+    document.querySelector('.player').innerHTML = span;
+  }
+
+  const win = (player) => {
+    nums = gameBoard.boardArray.filter(f => f.name === player.name).map(m => parseInt(m.num));
+    const combinations = [[1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]];
+    combinations.forEach((combination) => {
+      if (combination.every(f => nums.indexOf(f) > -1)) {
+        console.log("winn");
+        displayWiner(player.name);
+        return false;
+      }
+    })
+    return true;
+  }
+
   const play = (num) => {
     if (brd.checkSquare(num)) {
-      console.log({ turn });
       brd.fillSquare(turn, num);
-
-      turn = turn == player1 ? player2 : player1;
+      if (!win(turn)) {
+        turn = turn == player1 ? player2 : player1;
+        displayPlayer(turn);
+      }
     } else {
       alert("That square is already taken");
     }
@@ -49,6 +83,14 @@ const gameController = (() => {
       player1 = player(p1, "x");
       player2 = player(p2, "o");
       turn = player1;
+      displayPlayer(turn);
+      document.querySelectorAll(".main__container__square")
+        .forEach((square) => {
+          square.addEventListener("click", () => {
+            play(square.getAttribute('id'));
+          });
+        });
+
     } else {
       alert("you need to fill both names");
       return false;
