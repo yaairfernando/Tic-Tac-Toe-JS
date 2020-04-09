741c8e32df961c0a793f01d3dc8e03b014aeff98
Joseph Flores <yosept.flores@gmail.com>
2020-04-09 11:47:23 -0500

    We are trying to debug a problem where the board stops working if we clear it at the beggining

--- js/script.js
+++ js/script.js
@@ -15,22 +15,35 @@ const gameBoard = (() => {
     9: document.getElementById("9"),
   };
 
-  let boardArray = []
+  let boardArray = [];
 
-  const checkSquare = (num) => squares[num].innerHTML == "";
+  const checkSquare = (num) => squares[num].innerHTML == " ";
   const fillSquare = (player, num) => {
-    squares[num].innerHTML = player.mark;
     let move = {
       name: player.name,
-      num
-    }
+      num,
+    };
     boardArray.push(move);
+    squares[num].innerHTML = player.mark;
+    console.log("arr:", boardArray);
+  };
+
+  const clearBoard = () => {
+    console.log("enter");
+
+    document.querySelectorAll(".main__container__square").forEach((square) => {
+      square.innerHTML = " ";
+      square.style.display = "block";
+    });
+
+    let boardArray = [];
   };
 
   return {
     fillSquare,
     checkSquare,
-    boardArray
+    boardArray,
+    clearBoard,
   };
 })();
 
@@ -41,34 +54,59 @@ const gameController = (() => {
   let brd;
 
   const displayPlayer = (player) => {
-    let span = document.createElement('span').innerText = `It is ${player.name}'s turn`;
-    document.querySelector('.player').innerHTML = span;
-  }
+    let span = (document.createElement(
+      "span"
+    ).innerText = `It is ${player.name}'s turn`);
+    document.querySelector(".player").innerHTML = span;
+  };
 
   const displayWiner = (player) => {
-    let span = document.createElement('span').innerText = `The winner is ${player}`;
-    document.querySelector('.player').innerHTML = span;
-  }
+    console.log("won: ", player);
+
+    let winMessage = `
+    <h1 class="winMessage">Congratulations!, ${player} won!!!</h1>
+    `;
+
+    document.querySelectorAll(".main__container__square").forEach((square) => {
+      square.style.display = "none";
+    });
+    document.querySelector(".main__container").innerHTML += winMessage;
+  };
 
   const win = (player) => {
-    nums = gameBoard.boardArray.filter(f => f.name === player.name).map(m => parseInt(m.num));
-    const combinations = [[1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]];
+    console.log("gambrd: ", gameBoard);
+
+    nums = gameBoard.boardArray
+      .filter((f) => f.name === player.name)
+      .map((m) => parseInt(m.num));
+    const combinations = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
     combinations.forEach((combination) => {
-      if (combination.every(f => nums.indexOf(f) > -1)) {
-        console.log("winn");
+      console.log("wih: ", combination);
+      console.log("woh: ", nums);
+      if (combination.every((f) => nums.indexOf(f) > -1)) {
+        console.log("winn:", combination);
         displayWiner(player.name);
         return false;
       }
-    })
+    });
     return true;
-  }
+  };
 
   const play = (num) => {
     if (brd.checkSquare(num)) {
       brd.fillSquare(turn, num);
-      if (!win(turn)) {
+      if (win(turn)) {
         turn = turn == player1 ? player2 : player1;
+
         displayPlayer(turn);
+      } else {
+        return true;
       }
     } else {
       alert("That square is already taken");
@@ -79,18 +117,28 @@ const gameController = (() => {
     brd = board;
     p1 = document.getElementById("playerOne").value;
     p2 = document.getElementById("playerTwo").value;
+
+    document.getElementById("playerOne").value = "";
+    document.getElementById("playerTwo").value = "";
+
     if (p1 && p2) {
       player1 = player(p1, "x");
       player2 = player(p2, "o");
       turn = player1;
+
+      if (document.querySelector(".winMessage"))
+        document.querySelector(".winMessage").style.display = "none";
+
+      brd.clearBoard();
+
       displayPlayer(turn);
-      document.querySelectorAll(".main__container__square")
+      document
+        .querySelectorAll(".main__container__square")
         .forEach((square) => {
           square.addEventListener("click", () => {
-            play(square.getAttribute('id'));
+            play(square.getAttribute("id"));
           });
         });
-
     } else {
       alert("you need to fill both names");
       return false;
